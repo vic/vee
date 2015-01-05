d7bd9b140c369a2acd0d19f06e4e39157e7a6197
Victor Borja <vborja@apache.org>
2015-01-04 20:25:01 -0600

    Show magit status with popwin

--- init/init-popwin.el
+++ init/init-popwin.el
@@ -2,7 +2,7 @@
 (popwin-mode 1)
 
 (setq display-buffer-function 'popwin:display-buffer)
-(push '(magit-status-mode :regexp t :position :bottom :height 16 :dedicated t) popwin:special-display-config)
+(push '((lambda (b) (string-match-p "magit" (buffer-name b))) :tail nil :position :bottom :height 16 :dedicated t) popwin:special-display-config)
 (push '(term-mode :position :top :height 16 :dedicated t) popwin:special-display-config)
 (push '(" *undo-tree*" :width 0.3 :position right :dedicated t) popwin:special-display-config)
 
